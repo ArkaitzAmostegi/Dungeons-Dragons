4b2051282ed204d8fbd0243ba59a5746605d0fe0
Arkaitz <ikdge@plaiaundi.net>
2026-01-13 09:57:30 +0100

    Proxy inverso

--- src/vite.config.mjs
+++ src/vite.config.mjs
@@ -1,17 +1,22 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-
+import { defineConfig } from 'vite'
+import laravel from 'laravel-vite-plugin'
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
-    server: {
-        host: true,
-        port: 5173,
-        strictPort: true,
+  plugins: [
+    laravel({
+      input: ['resources/css/app.css', 'resources/js/app.js'],
+      refresh: true,
+    }),
+  ],
+  server: {
+    host: '0.0.0.0',     // para escuchar dentro del contenedor
+    port: 5173,
+    strictPort: true,
+    hmr: {
+      host: 'localhost', // lo que usará el navegador
+      port: 5173,
     },
-});
+    // en Windows/Docker a veces hace falta:
+    watch: { usePolling: true },
+  },
+})
