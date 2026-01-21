422634cb3b6390b9fe8e3f393c535125dfc5af54
Jokin Berridi <ikdgg@plaiaundi.com>
2026-01-21 13:54:09 +0100

    latidoPerfilAnimacion

--- backend/storage/framework/views/c590b94acd1cb2c42ea4fd93c6fbd7d7.php
+++ backend/storage/framework/views/c590b94acd1cb2c42ea4fd93c6fbd7d7.php
@@ -121,7 +121,11 @@
 <?php $component->withAttributes(['align' => 'right','width' => '48']); ?>
                      <?php $__env->slot('trigger', null, []); ?> 
                         <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
-                            <div><?php echo e(Auth::user()->name); ?></div>
+                            <div class="flex items-center">
+    <span class="user-status"></span>
+    <span><?php echo e(Auth::user()->name); ?></span>
+</div>
+
                             <div class="ms-1">
                                 <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                                     <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
